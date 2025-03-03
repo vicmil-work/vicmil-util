2372a0d77ea3db7b561486c65cab9d83ce8d2110
02vicmil <vicmil.millberg@gmail.com>
2025-03-03 20:50:09 +0100

    change example/mkdocs->example/docs and package name mkdocs->util_mkdocs to avoid conflicts with actual pip mkdocs package

--- example/mkdocs_buildme.py
+++ example/mkdocs_buildme.py
@@ -2,8 +2,8 @@ import sys
 from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parents[3])) # you may need to update the include path if run from somewhere else
 
-from vicmil_pip.mkdocs import *
+from vicmil_pip.util_mkdocs import *
 
-compile_mkdocs(get_directory(__file__) + "/mkdocs")
+compile_mkdocs(get_directory(__file__) + "/docs")
 
 
